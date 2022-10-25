d65bb1d6117f7b3aa5d54ef1225865d32ce6615c
Aleksandra Khrapkova <axi83@mail.ru>
2022-10-25 13:26:13 +0300

    генерирует тестовые данные

--- js/main.js
+++ js/main.js
@@ -1,14 +1,59 @@
-function getRandomIntegerNumber(a, b) {
+const AD_COUNT = 10;
+const LOCATION = {
+  LAT_MIN: 35.65000,
+  LAT_MAX: 35.70000,
+  LNG_MIN: 139.7000,
+  LNG_MAX: 139.8000,
+};
+const PRICE_MIN = 1000;
+const PRICE_MAX = 9999;
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TIME = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const getRandomIntegerNumber = (a, b) => {
   const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-function getRandomFloatNumber(a, b, decimalPlaces = 2) {
+const getRandomFloatNumber = (a, b, decimalPlaces = 5) => {
   const min = Math.min(Math.abs(a), Math.abs(b));
   const max = Math.max(Math.abs(a), Math.abs(b));
   return +(Math.random() * (max - min) + min).toFixed(decimalPlaces);
-}
+};
 
-getRandomIntegerNumber(0, 3);
-getRandomFloatNumber(1.197, 1.200, 3);
+const getRandomArrayElement = (array) => array[getRandomIntegerNumber(0, array.length - 1)];
+
+const createAd = (index) => ({
+  author: {
+    avatar: `img/avatars/user${index.toString().length === 1 ? `0${index}` : index }.png`,
+  },
+  offer: {
+    title: 'Заголовок предложения',
+    // Как получить доступ к свойству объекта и другого объекта в общем объекте
+    address:  `${this.location.lat} ${this.location.lat}`,
+    price: getRandomIntegerNumber(PRICE_MIN, PRICE_MAX),
+    type: getRandomArrayElement(TYPES),
+    rooms: getRandomIntegerNumber(1, 5),
+    guests: getRandomIntegerNumber(1, 10),
+    checkin: getRandomArrayElement(TIME),
+    checkout: getRandomArrayElement(TIME),
+    features: Array.from({length: getRandomIntegerNumber(1, FEATURES.length)},
+      (_, i) => FEATURES[i]),
+    description: 'Описание помещения',
+    photos: Array.from({length: getRandomIntegerNumber(1, 3)}, (_, i) => PHOTOS[i])
+  },
+  location: {
+    lat: getRandomFloatNumber(LOCATION.LAT_MIN, LOCATION.LAT_MAX),
+    lng: getRandomFloatNumber(LOCATION.LNG_MIN, LOCATION.LNG_MAX),
+  }
+});
+
+const createAds = Array.from({length: AD_COUNT}, (_, index) => createAd(index + 1));
+console.log(createAds);
